5009ae3e79211abdaab4f916b3c4aa3d11cf6228
raghuhr18 <raghuhr18@yahoo.com>
2024-01-18 19:16:22 +0530

    updating the restaurants by Fetch API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { restaurantList } from "../config";
 import RestaurantCard from "../RestaurantCard";
 
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.info.name.includes(searchText)
+  );
+  return filterData;
+}
+
 const Body = () => {
   const [searchText, setSearchText] = useState("");
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
+  useEffect(() => {
+    getRestaurantData();
+  }, []);
+
+  async function getRestaurantData() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3108256&lng=78.03444259999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  }
   return (
     <>
       <div className="seach-container">
@@ -15,10 +38,18 @@ const Body = () => {
           onChange={(e) => setSearchText(e.target.value)}
         />
 
-        <button>Search</button>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterData(searchText, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="restaurant-list">
-        {restaurantList.map((restaurant) => {
+        {restaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant?.info} key={restaurant?.info?.id} />
           );
