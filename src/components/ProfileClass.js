45e178b68d02440e5a10da0df2349508032f11b2
raghuhr18 <raghuhr18@yahoo.com>
2024-01-24 23:15:39 +0530

    updated sequence

--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -6,17 +6,21 @@ class ProfileClass extends React.Component {
     this.state = {
       count: 0,
     };
+    console.log("Constructor");
+  }
+  
+  componentDidMount() {
+    console.log("Component Did Mount");
   }
   render() {
     const { count } = this.state;
+    console.log("render");
     return (
       <div>
         
         <h1>This is profile class component</h1>
         <h2>{count}</h2>
-        <button onClick={() => {
-            this.setState({count: count+1})
-        }}>Click</button>
+        <button onClick={() => {this.setState({count: count+1})}}>Click Here</button>
       </div>
     )
   }
