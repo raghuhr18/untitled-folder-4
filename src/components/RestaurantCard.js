7b4d731bdc22c73b56dd8cc28f13c8ed37e1bcf2
raghuhr18 <raghuhr18@yahoo.com>
2024-02-11 23:59:20 +0530

    css changes updated

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,8 +5,8 @@ const RestaurantCard = ({
   avgRatingString,
 }) => {
   return (
-    <div className="w-56 shadow-md p-2 m-2 border border-l">
-      <img
+    <div className="w-60 shadow-md p-2 m-2 border border-l h-80 bg-pink-100">
+      <img className="h-48 object-fill w-full"
         src={
           "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
           cloudinaryImageId
